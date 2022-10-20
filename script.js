fb58084864eab6e2fc8e5dd7ca454a80ccbfae32
ireoz <andrew.higgins2@hotmail.com>
2022-10-20 19:26:18 +0100

    add functionality so that progress bar increments when audio is playing

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const playPause = document.getElementById('playPause');
 const audio = document.querySelector('audio');
 const currentTime = document.getElementById('current-time');
 const duration = document.getElementById('duration');
+const progressBar = document.getElementById('progress');
+
 
 
 let milliseconds = 0;
 let seconds = 0;
 let minutes = 0;
-let songTotalTime = 0;
+let audioCurrentTime = 0;
+let audioTotalTime = 0;
+let progressPercentage = 0;
 
 
 
@@ -42,8 +46,9 @@ function displayCurrentTime(){
    seconds >= 60 ? minutes = (seconds/60).toFixed(0) : 0;
    minutes >= 1 ? seconds = seconds % 60 : 0;
    seconds < 10 ? seconds = '0' + seconds : seconds;
-   let audioTime = `${minutes}:${seconds}`;
-   currentTime.innerHTML = audioTime;
+   audioCurrentTime = `${minutes}:${seconds}`;
+   currentTime.innerHTML = audioCurrentTime;
+   animateProgressBar();
 
 }
 
@@ -52,10 +57,17 @@ function displayTotalTime() {
   let tS = audio.duration.toFixed(0) % 60;
    let tM = (audio.duration/60).toFixed(0);
    tS < 10 ? tS = '0' + tS : tS;
-   songTotalTime = `${tM}:${tS}`
-   console.log(songTotalTime, tS, tM);
-   duration.innerHTML = songTotalTime;
+   audioTotalTime = `${tM}:${tS}`;
+   duration.innerHTML = audioTotalTime;
 
+}
+
+// progress bar animation 
+function animateProgressBar() {
+   let songCurrentTime = audio.currentTime;
+   let songTotalTime = audio.duration;
+   progressPercentage = ((songCurrentTime/songTotalTime)*100).toFixed(0);
+   progressBar.style.width = progressPercentage + '%'
 }
 
 // Event listeners
