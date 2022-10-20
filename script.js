07bb8c8c9e89eb0bc574e8f0270d22ec9b7e7447
ireoz <andrew.higgins2@hotmail.com>
2022-10-20 18:24:03 +0100

    add functionality to set the song total time to correct format and display on music player on load of data

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ const duration = document.getElementById('duration');
 
 
 let milliseconds = 0;
-    let seconds = 0;
-    let minutes = 0;
+let seconds = 0;
+let minutes = 0;
+let songTotalTime = 0;
 
 
 
@@ -34,18 +35,30 @@ function isPlaying() {
         setInterval(displayCurrentTime, 1000)
         
     }
-//  set correct time format and write current time to web page
+//  set correct time format and write current time to the music player
 function displayCurrentTime(){
-
+   
    seconds = audio.currentTime.toFixed(0);
    seconds >= 60 ? minutes = (seconds/60).toFixed(0) : 0;
-   minutes >= 1 ? seconds = seconds - 60 : 0;
+   minutes >= 1 ? seconds = seconds % 60 : 0;
    seconds < 10 ? seconds = '0' + seconds : seconds;
    let audioTime = `${minutes}:${seconds}`;
    currentTime.innerHTML = audioTime;
 
 }
 
+// set correct time format and write song total time to the music player
+function displayTotalTime() { 
+  let tS = audio.duration.toFixed(0) % 60;
+   let tM = (audio.duration/60).toFixed(0);
+   tS < 10 ? tS = '0' + tS : tS;
+   songTotalTime = `${tM}:${tS}`
+   console.log(songTotalTime, tS, tM);
+   duration.innerHTML = songTotalTime;
+
+}
+
 // Event listeners
 playPause.addEventListener('click', togglePlayPauseButton);
 audio.addEventListener('play', isPlaying);
+audio.addEventListener('loadeddata', displayTotalTime);
